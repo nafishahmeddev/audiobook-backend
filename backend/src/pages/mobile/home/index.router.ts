fc6aea60a75879d0f40a93736b51a095be08929c
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-10-26 11:58:15 +0530

    [UPDATE]

--- backend/src/pages/mobile/home/index.router.ts
+++ backend/src/pages/mobile/home/index.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Album, Track } from "../../../db/default";
+import { Album, Author, Genre, List, Track } from "../../../db/default";
 import ResponseHelper from "../../../helpers/ResponseHelper";
 
 const router = Router({ mergeParams: true });
@@ -7,6 +7,11 @@ function baseUrl(_path: string): string {
     return process.env.PUBLIC_URL + _path;
 }
 router.get("/", async (req: any, res: any) => {
+
+    const genres = await Genre.aggregate([{ $sample: { size: 10 } }]);
+    const lists = await List.aggregate([{ $sample: { size: 6 } }]);
+    const authors = await Author.aggregate([{ $sample: { size: 10 } }]);
+
     const albums = await Album.aggregate([
         { $sample: { size: 10 } },
         { $limit: 10 },
@@ -30,10 +35,11 @@ router.get("/", async (req: any, res: any) => {
         code: 200,
         message: "Cool",
         payload: {
-            trending: {
-                albums: albums.map(album => ({ ...album, thumbnail: baseUrl(album.thumbnail) })),
-                tracks: tracks.map(track => ({ ...track, thumbnail: baseUrl(track.thumbnail), audio: baseUrl(track.audio) })),
-            }
+            lists: lists.map(list => ({ ...list.toObject(), thumbnail: baseUrl(list.thumbnail) })),
+            genres: genres.map(genre => ({ ...genre.toObject(), thumbnail: baseUrl(genre.thumbnail) })),
+            authors: authors.map(author => ({ ...author.toObject(), image: baseUrl(author.image) })),
+            albums: albums.map(album => ({ ...album, thumbnail: baseUrl(album.thumbnail) })),
+            tracks: tracks.map(track => ({ ...track, thumbnail: baseUrl(track.thumbnail), audio: baseUrl(track.audio) })),
         }
     }));
 })
