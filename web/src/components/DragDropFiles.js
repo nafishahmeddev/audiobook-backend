ca5bb429423a2dbfc306025167d06f0680f38261
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-02-27 19:34:36 +0530

    [UPDATE]

--- web/src/components/DragDropFiles.js
+++
@@ -1,3 +0,0 @@
-export { default as ChapterUploader } from './ChapterUploader';
-export { default as ImageUploader } from './ImageUploader';
-export { default as StaticFileUploader } from './StaticFileUploader';
--- web/src/services/admin/DashboardServices.js
+++
@@ -1,7 +0,0 @@
-import Request from "@app/lib/request";
-const request = new Request("/admin/dashboard");
-export default class DashboardServices {
-    static stats = (from, to) => request.http.post("/stats", { from, to });
-    static all = () => request.http.get("/")
-    static get = (_id) => request.http.get(`/${_id}`)
-}
