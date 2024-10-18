0b5e8497047204939c0ca483081982653d1b3369
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-10-18 17:22:07 +0530

    chore:updated dummy generation

--- backend/src/pages/mobile/faker.router.ts
+++ backend/src/pages/mobile/faker.router.ts
@@ -1,147 +1,202 @@
 import { Router } from "express";
 import { Album, Author, Genre, List, Track } from "../../db/default";
 import ResponseHelper from "../../helpers/ResponseHelper";
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 import axios from "axios";
 import fs from "fs";
 import { ALBUM_TYPE_ENUM, TRACK_TYPE_ENUM } from "../../enums/album";
 import { exec } from "child_process";
-
+import ffmpeg from "fluent-ffmpeg";
 
 const router = Router({ mergeParams: true });
 function art(): string {
-    const arts = fs.readdirSync(process.env.PROJECT_PATH + "/dummy/cover");
-    return process.env.PROJECT_PATH + "/dummy/cover" + "/" + arts[Math.floor(Math.random() * arts.length)];
+  const arts = fs.readdirSync(process.env.PROJECT_PATH + "/dummy/cover");
+  return (
+    process.env.PROJECT_PATH +
+    "/dummy/cover" +
+    "/" +
+    arts[Math.floor(Math.random() * arts.length)]
+  );
 }
 function userPhoto(): string {
-    const arts = fs.readdirSync(process.env.PROJECT_PATH + "/dummy/avatar");
-    return process.env.PROJECT_PATH + "/dummy/avatar" + "/" + arts[Math.floor(Math.random() * arts.length)];
+  const arts = fs.readdirSync(process.env.PROJECT_PATH + "/dummy/avatar");
+  return (
+    process.env.PROJECT_PATH +
+    "/dummy/avatar" +
+    "/" +
+    arts[Math.floor(Math.random() * arts.length)]
+  );
 }
 router.patch("/generate", async (req: any, res: any) => {
-    // return res.send("Currently turned off....");
-    fs.readdirSync(`${process.env.ASSETS_PATH}/public/images`).forEach(e => fs.rmSync(`${process.env.ASSETS_PATH}/public/images/` + e));
-    fs.readdirSync(`${process.env.ASSETS_PATH}/public/audios`).forEach(e => fs.rmSync(`${process.env.ASSETS_PATH}/public/audios/` + e));
+  res.status(200).json(
+    ResponseHelper.success({
+      code: 200,
+      message: "Successful",
+    })
+  );
 
-    await Author.deleteMany({ _id: { $ne: null } });
-    await Genre.deleteMany({ _id: { $ne: null } });
-    await List.deleteMany({ _id: { $ne: null } });
-    await Album.deleteMany({ _id: { $ne: null } });
-    await Track.deleteMany({ _id: { $ne: null } });
+  // return res.send("Currently turned off....");
+  fs.readdirSync(`${process.env.ASSETS_PATH}/public/images`).forEach((e) =>
+    fs.rmSync(`${process.env.ASSETS_PATH}/public/images/` + e)
+  );
+  fs.readdirSync(`${process.env.ASSETS_PATH}/public/audios`).forEach((e) =>
+    fs.rmSync(`${process.env.ASSETS_PATH}/public/audios/` + e, { recursive: true })
+  );
 
-    console.log("Generating authors....")
-    for (let n = 0; n < 20; n++) {
-        const author = new Author({
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            gender: faker.person.gender(),
-            description: faker.lorem.sentence(),
-            dob: Date(),
-            dod: Date(),
-        });
-        const filepath = `${process.env.ASSETS_PATH}/public/images/author-${author._id.toString()}-${Date.now()}.jpg`;
-        fs.cpSync(userPhoto(), filepath);
-        author.image = filepath.replace(process.env.ASSETS_PATH ?? "", "");
-        await author.save();
-        console.log(`Generated author ${n}....`)
+  await Author.deleteMany({ _id: { $ne: null } });
+  await Genre.deleteMany({ _id: { $ne: null } });
+  await List.deleteMany({ _id: { $ne: null } });
+  await Album.deleteMany({ _id: { $ne: null } });
+  await Track.deleteMany({ _id: { $ne: null } });
+
+  console.log("Generating authors....");
+  for (let n = 0; n < 20; n++) {
+    const author = new Author({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      gender: faker.person.gender(),
+      description: faker.lorem.sentence(),
+      dob: Date(),
+      dod: Date(),
+    });
+    const filepath = `${process.env.ASSETS_PATH
+      }/public/images/author-${author._id.toString()}-${Date.now()}.jpg`;
+    fs.cpSync(userPhoto(), filepath);
+    author.image = filepath.replace(process.env.ASSETS_PATH ?? "", "");
+    await author.save();
+    console.log(`Generated author ${n}....`);
+  }
+
+  console.log("Generating genre....");
+  for (let n = 0; n < 10; n++) {
+    const genre =
+      (await Genre.findOne({
+        name: faker.music.genre(),
+      })) ??
+      new Genre({
+        name: faker.music.genre(),
+      });
+
+    const filepath = `${process.env.ASSETS_PATH
+      }/public/images/genre-${genre._id.toString()}-${Date.now()}.jpg`;
+    fs.cpSync(art(), filepath);
+    genre.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
+    await genre.save();
+    console.log(`Generated genres ${n}....`);
+  }
+
+  console.log("Generating lists....");
+  for (let n = 0; n < 20; n++) {
+    const list = new List({
+      name: faker.company.name(),
+      subtitle: faker.commerce.department(),
+    });
+    const filepath = `${process.env.ASSETS_PATH
+      }/public/images/list-${list._id.toString()}-${Date.now()}.jpg`;
+    fs.cpSync(art(), filepath);
+    list.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
+    await list.save();
+    console.log(`Generated list ${n}....`);
+  }
+
+  console.log("Generating albums....");
+  for (let n = 0; n < 30; n++) {
+    const album: any = new Album({
+      title: faker.company.name(),
+      excerpt: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      language: "en",
+      duration: Math.ceil(Math.random() * 100000),
+      authors: await Author.aggregate([{ $sample: { size: 1 } }]),
+      genres: await Genre.aggregate([{ $sample: { size: 1 } }]),
+      lists: await List.aggregate([{ $sample: { size: 1 } }]),
+      type: ALBUM_TYPE_ENUM.ALBUM,
+    });
+    const filepath = `${process.env.ASSETS_PATH
+      }/public/images/album-${album._id.toString()}-${Date.now()}.jpg`;
+    fs.cpSync(art(), filepath);
+    album.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
+    await album.save();
+    console.log(`Generated album ${n}....`);
+  }
+
+  console.log("Generating tracks....");
+  for (let n = 0; n < 100; n++) {
+    const track: any = new Track({
+      title: faker.music.songName(),
+      excerpt: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      language: "en",
+      duration: Math.ceil(Math.random() * 100000),
+      authors: await Author.aggregate([{ $sample: { size: 2 } }]),
+      genres: await Genre.aggregate([{ $sample: { size: 2 } }]),
+      lists: await List.aggregate([{ $sample: { size: 2 } }]),
+      type: TRACK_TYPE_ENUM.SONG,
+      album: (await Album.aggregate([{ $sample: { size: 1 } }]))[0],
+    });
+    const filepath = `${process.env.ASSETS_PATH}/public/images/track-${track._id.toString()}-${Date.now()}.jpg`;
+    fs.cpSync(art(), filepath);
+    track.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
+
+    const sourcePath = `${process.env.PROJECT_PATH}/dummy/audio/test.mp3`;
+    const outPath = `${process.env.ASSETS_PATH}/public/audios/streams/${track._id.toString()}`;
+    const m3u8Path = `${outPath}/index.m3u8`;
+
+    fs.mkdirSync(outPath, { recursive: true });
+
+    try {
+      await new Promise((resolve, reject) => {
+        ffmpeg(sourcePath, { timeout: 432000 })
+          .addOptions([
+            "-map 0:a",
+            "-c:a aac",
+            "-b:a 128k",
+            "-f hls",
+            "-hls_time 10",
+            "-hls_list_size 0",
+            `-hls_segment_filename ${outPath}/chunk%d.ts`,
+          ])
+          .saveToFile(m3u8Path)
+          .on("error", (error) => {
+            console.error(`exec error: ${error.message}`);
+            reject(false);
+          })
+          .on("progress", (command) => {
+            console.log("exec progress: ", command);
+          })
+          .on("end", (stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            resolve(true);
+          });
+        //   exec(
+        //     `ffmpeg -i ${sourcepath}  -c:a aac -b:a 192k  ${audiopath}`,
+        //     (error, stdout, stderr) => {
+        //       if (error) {
+        //         console.error(`exec error: ${error}`);
+        //         reject(false);
+        //         return;
+        //       }
+        //       console.log(`stdout: ${stdout}`);
+        //       console.error(`stderr: ${stderr}`);
+        //       resolve(true);
+        //     }
+        //   );
+      });
+
+      track.audio = m3u8Path.replace(process.env.ASSETS_PATH ?? "", "");
+      await track.save();
+      console.log(`Generated track ${n}....`);
+    } catch (err) {
+      console.log(`Generated not possible track ${n}....`);
     }
+  }
 
-
-    console.log("Generating genre....")
-    for (let n = 0; n < 10; n++) {
-        const genre = await Genre.findOne({
-            name: faker.music.genre(),
-        }) ?? new Genre({
-            name: faker.music.genre(),
-        });
-
-        const filepath = `${process.env.ASSETS_PATH}/public/images/genre-${genre._id.toString()}-${Date.now()}.jpg`;
-        fs.cpSync(art(), filepath);
-        genre.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
-        await genre.save();
-        console.log(`Generated genres ${n}....`)
-    }
-
-    console.log("Generating lists....")
-    for (let n = 0; n < 20; n++) {
-        const list = new List({
-            name: faker.company.name(),
-            subtitle: faker.commerce.department()
-        });
-        const filepath = `${process.env.ASSETS_PATH}/public/images/list-${list._id.toString()}-${Date.now()}.jpg`;
-        fs.cpSync(art(), filepath);
-        list.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
-        await list.save();
-        console.log(`Generated list ${n}....`)
-    }
-
-    console.log("Generating albums....")
-    for (let n = 0; n < 30; n++) {
-        const album: any = new Album({
-            title: faker.company.name(),
-            excerpt: faker.lorem.sentence(),
-            description: faker.lorem.sentence(),
-            language: "en",
-            duration: Math.ceil(Math.random() * 100000),
-            authors: await Author.aggregate([{ $sample: { size: 1 } }]),
-            genres: await Genre.aggregate([{ $sample: { size: 1 } }]),
-            lists: await List.aggregate([{ $sample: { size: 1 } }]),
-            type: ALBUM_TYPE_ENUM.ALBUM
-        });
-        const filepath = `${process.env.ASSETS_PATH}/public/images/album-${album._id.toString()}-${Date.now()}.jpg`;
-        fs.cpSync(art(), filepath);
-        album.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
-        await album.save();
-        console.log(`Generated album ${n}....`)
-    }
-
-
-    console.log("Generating tracks....")
-    for (let n = 0; n < 100; n++) {
-        const track: any = new Track({
-            title: faker.music.songName(),
-            excerpt: faker.lorem.sentence(),
-            description: faker.lorem.sentence(),
-            language: "en",
-            duration: Math.ceil(Math.random() * 100000),
-            authors: await Author.aggregate([{ $sample: { size: 2 } }]),
-            genres: await Genre.aggregate([{ $sample: { size: 2 } }]),
-            lists: await List.aggregate([{ $sample: { size: 2 } }]),
-            type: TRACK_TYPE_ENUM.SONG,
-            album: (await Album.aggregate([{ $sample: { size: 1 } }]))[0]
-        });
-        const filepath = `${process.env.ASSETS_PATH}/public/images/track-${track._id.toString()}-${Date.now()}.jpg`;
-        fs.cpSync(art(), filepath);
-        track.thumbnail = filepath.replace(process.env.ASSETS_PATH ?? "", "");
-
-        const sourcepath = `${process.env.PROJECT_PATH}/dummy/audio/test.mp3`;
-        const audiopath = `${process.env.ASSETS_PATH}/public/audios/track-${track._id.toString()}-${Date.now()}.m4a`;
-
-        try {
-            await new Promise((resolve, reject) => {
-                exec(`ffmpeg -i ${sourcepath}  -c:a aac -b:a 192k  ${audiopath}`, (error, stdout, stderr) => {
-                    if (error) {
-                        console.error(`exec error: ${error}`);
-                        reject(false);
-                        return;
-                    }
-                    console.log(`stdout: ${stdout}`);
-                    console.error(`stderr: ${stderr}`);
-                    resolve(true);
-                });
-            })
-
-            track.audio = audiopath.replace(process.env.ASSETS_PATH ?? "", "");
-            await track.save();
-            console.log(`Generated track ${n}....`)
-        } catch (err) {
-            console.log(`Generated not possible track ${n}....`)
-        }
-    }
-
-    return res.status(200).json(ResponseHelper.success({
-        code: 200,
-        message: "Successful",
-    }));
-})
+  // return res.status(200).json(
+  //   ResponseHelper.success({
+  //     code: 200,
+  //     message: "Successful",
+  //   })
+  // );
+});
 
 export default router;
