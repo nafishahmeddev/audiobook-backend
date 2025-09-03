72e130301deb94656359b4c91f8a86fbd2a2a493
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-09-03 11:02:26 +0530

    chore: remove frontend static file serving from index.ts and clean up .env-example

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,8 +23,6 @@ async function boot() {
 
 boot().then(() => {
   app.use("/public", express.static(path.join(process.env.ASSETS_PATH || "", "public")));
-  app.use(express.static(path.join(process.env.FRONTEND_PATH || "dist")));
-  app.use("*", express.static(path.join(process.env.FRONTEND_PATH || "", "dist", "index.html")));
   app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
   });
